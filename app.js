4884138fa4753b5db2d9aa18538ddfabc671713b
ikamean <ika.bidzishvili@gmail.com>
2021-03-04 05:45:50 +0400

    added express route * for handling any request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -7,7 +8,9 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5005
 
-app.use(express.static('build'))
+
+app.use(express.static('build'));
+app.get("*", (req, res) => { res.sendFile(path.join( __dirname + "/build/index.html" )); });
 
 
 // eslint-disable-next-line no-console
