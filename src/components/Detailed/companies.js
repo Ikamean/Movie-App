0d6513d9344a7f943c45adeb8979e336d290f880
ikamean <ika.bidzishvili@gmail.com>
2021-03-05 16:56:24 +0400

    added hoverable text and extendable image

--- src/components/Detailed/companies.js
+++ src/components/Detailed/companies.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
-import {getCompanyHomepage} from '../../services/service';
+import { getCompanyHomepage } from '../../services/service';
 
 const Company = ({ company }) => {
 
--- src/components/Homepage/movieCard.js
+++ src/components/Homepage/movieCard.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { AiFillStar } from 'react-icons/ai';
+import { MdLanguage } from 'react-icons/md';
+import { GiExpand } from 'react-icons/gi';
 
 
 import styled from 'styled-components';
 
 
 const MovieCard = ({ movie }) => {
+    const [ openImage, setOpenImage ] = useState(false)
+
     const imgBaseUrl = useSelector( state => state.config.imagesConfig.secure_base_url);
     const history = useHistory();
 
-
+    console.log(movie);
     
 
     const handleClick = async () => {
@@ -21,9 +26,47 @@ const MovieCard = ({ movie }) => {
     
     return(
         <Card>
-            <div onClick={()=>handleClick()}>
+            
+            <div>
+                <ExpandBtn onClick={()=>setOpenImage(!openImage)}>
+                    <GiExpand />
+                </ExpandBtn>
+               
+                <div onClick={()=>handleClick()}>
+                    {
+                    openImage && 
+                    <Poster loading='lazy' 
+                    src={ `${imgBaseUrl}w300${movie.poster_path}`}
+                    alt='Movie Poster'/>
+                    }   
+                </div>
+                
+                
+                <HoverableImage onClick={()=>handleClick()}>
+
+                <HoverableText>
+                        <HoverableTextContainer>
+
+                            <div>
+                                {movie.original_title}
+                            </div>
+
+                            <div>
+                                <AiFillStar /> {movie.vote_average}
+                            </div>
+
+                            <div>
+                                <MdLanguage /> {movie.original_language}
+                            </div>
+
+                            
+                            
+                        </HoverableTextContainer>
+                    
+                </HoverableText>
                 {
-                    movie.poster_path ? 
+                    movie.poster_path  ?
+                    !openImage && 
                     <picture>
                         <source media="(min-width:650px)" srcSet={ `${imgBaseUrl}w154${movie.poster_path}`} />
 
@@ -34,11 +77,13 @@ const MovieCard = ({ movie }) => {
                     </picture>
 
                     : 
-
+                        !openImage && 
                         <NoPoster src={require('../../Media/noposter.svg').default} width="92" height="138" alt='No Poster Available' />
                     
-                } 
+                }
+                
                 <Title>{movie.title}</Title>
+                </HoverableImage>
             </div>
                 <MovieDate>{movie.release_date}</MovieDate>
             
@@ -49,6 +94,71 @@ const MovieCard = ({ movie }) => {
 }
 
 export default MovieCard;
+
+const ExpandBtn = styled.button`
+    display: none;
+    @media(min-width: 650px){
+        display: block;
+        background-color: transparent;
+        color: black;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        &:hover{
+            font-size: 1rem;
+        }
+    }
+    
+
+`
+
+const HoverableText = styled.div`
+    display: none;
+    position: absolute;
+    color: black;
+    left: 92px;
+    z-index: 1;
+    background-color: white;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    font-weight: 400;
+    font-size: 0.8rem;
+    width: 100px;
+    height: 100px;
+    border: none;
+    outline: none;
+    font-weight: 600;
+    @media(min-width: 650px){
+        left: 154px;
+    }
+`
+
+const HoverableImage = styled.div`
+    position: relative;
+
+    @media(min-width: 650px){
+
+        &:hover ${ HoverableText }{
+        display: block;
+        transition: 0.5s all ease;
+    }
+    }
+    @media(max-width: 650px){
+        &:focus ${ HoverableText }{
+        display: block;
+        transition: 0.5s all ease;
+    }
+    }
+    
+`
+const HoverableTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: flex-start;
+    height: 100%;
+`
+
 
 export const Title = styled.h3`
     font-weight: 700;
@@ -73,9 +183,6 @@ export const MovieDate = styled.p`
 export const Poster = styled.img`
     border-radius: 8px;
     cursor: pointer;
-    &:hover{
-        opacity: 0.8;
-    }
     
 `
 export const NoPoster = styled.img`
@@ -87,7 +194,7 @@ export const NoPoster = styled.img`
 `
 
 const Card = styled.div`
-    cursor: pointer;
+    position: relative;
     border-radius: 30px;
     padding: 1rem;
     display: flex;
